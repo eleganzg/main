09f7a00d63dc5b42ce73a9b4fdf0ec88c99a96b2
Rafael Omar Ortiz Galaviz <rafael.ortiz@hidalgo.dextratech.com>
2017-05-16 12:20:03 -0500

    Add initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header';
+import { FooterComponent } from './footer';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        FooterComponent
     ],
     imports: [
         BrowserModule,
